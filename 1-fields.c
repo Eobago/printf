58b3253845bba01f9150ffe3f5bd27c382a59344
Lemaekeejimofor101 <lemaekeejimofor@gmail.com>
2023-04-23 18:08:13 -0700

    correct git commit -m correct

--- 1-fields.c
+++ 1-fields.c
@@ -11,11 +11,11 @@ void init_params(params_t *params, va_list ap)
 {
 	params->unsign = 0;
 
-	params->plus_fields = 0;
-	params->space_fields = 0;
-	params->hashtag_fields = 0;
-	params->zero_fields = 0;
-	params->minus_fields = 0;
+	params->plus_flag = 0;
+	params->space_flag = 0;
+	params->hashtag_flag = 0;
+	params->zero_flag = 0;
+	params->minus_flag = 0;
 
 	params->width = 0;
 	params->precision = UINT_MAX;
